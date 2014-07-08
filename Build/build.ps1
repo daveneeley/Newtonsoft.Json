bf22c5378207f08927694300424e8102e7610ddf
buildmas <buildmasters@spillman.com>
2014-07-08 15:11:55 -0600

    Made changes so that the file name for each of the dll's is appended with the assembly version.

--- Build/build.ps1
+++ Build/build.ps1
@@ -87,6 +87,8 @@ task Package -depends Build {
     $finalDir = $build.FinalDir
     
     robocopy "$sourceDir\Newtonsoft.Json\bin\Release\$finalDir" $workingDir\Package\Bin\$finalDir *.dll *.pdb *.xml /NP /XO /XF *.CodeAnalysisLog.xml | Out-Default
+	$completeAssemblyVersion = Get-ChildItem "$sourceDir\Newtonsoft.Json\bin\Release\$finalDir" | Get-ItemProperty -Name VersionInfo | Select-String -Pattern "ProductVersion:\s+(.*)" -All | %{ $_.matches } | %{ $_.Value } | Select-String -pattern "[0-9.]+" -All | %{ $_.matches } | %{ $_.Value }
+	Get-ChildItem "$sourceDir\Newtonsoft.Json\bin\Release\$finalDir\\*.dll" | Rename-Item -NewName { ($_.BaseName, $completeAssemblyVersion, $_.Extension) -join "" }
   }
   
   if ($buildNuGet)
